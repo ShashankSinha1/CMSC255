f553b9fb875ab8a7c81185146e15f7cabd6045c6
ShashankSinha1 <117942824+ShashankSinha1@users.noreply.github.com>
2022-11-10 14:39:20 -0500

    Add files via upload

---
+++ VariableDefinitions.java
@@ -0,0 +1,96 @@
+public class VariableDefinitions
+{
+	public static void main(String[] args)
+	{
+	
+/*************************************
+* initial declaration of numPeople variable
+****************************************/
+		byte numPeople = 3;
+		
+		System.out.println("The numPeople variable has a value of " + numPeople);
+		
+/*************************************
+* modified value of the numPeople variable
+****************************************/
+		numPeople = 6;
+		
+		System.out.println("The numPeople variable now has a value of " + numPeople);
+		
+/*************************************
+* initial declaration of itemPrice variable
+****************************************/
+		double itemPrice = 32.32;
+		
+		System.out.println("The itemPrice variable has a value of " + itemPrice);
+		
+/*************************************
+* modified value of the itemPrice variable
+****************************************/
+		itemPrice = 25.25;
+		
+		System.out.println("The numPeople variable now has a value of " + numPeople);
+		
+		
+/*************************************
+* initial declaration of classroomNumber variable
+****************************************/
+		int classroomNumber = 207;
+		
+		System.out.println("The classroomNumber variable has a value of " + classroomNumber);
+		
+		
+/*************************************
+* modified value of the classroomNumber variable
+****************************************/
+		classroomNumber = 189;
+		
+		System.out.println("The classroomNumber variable now has a value of " + classroomNumber);
+		
+		
+/*************************************
+* initial declaration of dinnerPrice variable
+****************************************/
+		float dinnerPrice = 30.0F;
+		
+		System.out.println("The dinnerPrice variable has a value of " + dinnerPrice);
+		
+/*************************************
+* modified value of the dinnerPrice variable
+****************************************/
+		dinnerPrice = 36.9F;
+		
+		System.out.println("The dinnerPrice variable now has a value of " + dinnerPrice);
+		
+/*************************************
+* initial declaration of numPeople constant
+****************************************/
+		final byte NUM_PEOPLE = 42; 
+		
+/*************************************
+* initial declaration of itemPrice constant
+****************************************/
+		final double ITEM_PRICE = 40.01;
+
+/*************************************
+* initial declaration of classroomNumber constant
+****************************************/
+		final int CLASSROOM_NUMBER = 200;
+		
+/*************************************
+* initial declaration of dinnerPrice constant
+****************************************/
+		final float DINNER_PRICE = 50;
+		
+		System.out.println(NUM_PEOPLE);
+		System.out.println(ITEM_PRICE);
+		System.out.println(CLASSROOM_NUMBER);
+		System.out.println(DINNER_PRICE);
+
+//NUM_PEOPLE = 12;
+		
+	}
+}
+		
+	
+	
